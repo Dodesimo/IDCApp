ba2fe06fe612a19bbab2e40192c8054225a1c344
Devam Mondal <devammondal06@gmail.com>
2022-09-11 19:59:51 -0400

    finished app structure

--- main.py
+++ main.py
@@ -1,20 +1,80 @@
 import flask
-from flask import Flask, render_template, request
+import torchvision.transforms
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
+import torch
+import pandas as pd
+from torch.utils.data import Dataset
+from skimage import io
+from skimage.transform import resize
+import os
+from torch.utils.data import DataLoader
+from torch import nn
+import numpy as np
+from torchvision.transforms import ToTensor
+from matplotlib import pyplot as plt
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.c1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=(5, 5), stride=5)
+        self.r1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.c2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(2, 2), stride=1)
+        self.r2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(3, 3), stride=1)
+        self.lin1 = nn.Linear(2 * 2 * 20, 150)
+        self.lin2 = nn.Linear(150, 2)
+        self.relu = nn.ReLU()
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        x = self.c1(x)
+        x = self.r1(x)
+        x = self.maxpool1(x)
+        x = self.c2(x)
+        x = self.r2(x)
+        x = self.maxpool2(x)
+        x = x.flatten(1)
+        x = self.lin1(x)
+        x = self.lin2(x)
+        x = self.relu(x)
+        x = self.logsoftmax(x)
+        return torch.exp(x)
+
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = '/uploads'
+app.config['UPLOAD_FOLDER'] = '/Users/devam/PycharmProjects/IDCApp/uploads'
 app.config['MAX_CONTENT_PATH'] = 10000
 
-@app.route('/', methods = ['GET', 'POST'])
+
+@app.route('/', methods=["GET", "POST"])
 def main():
     if request.method == 'POST':
-        f = request.files['f']
-        f.save(secure_filename(f.filename))
-        return flask.render_template('finished.html')
+        f = request.files['file']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+
+        model = Net()
+
+        image = io.imread(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        resize(image, (50, 50))
+        torchvision.transforms.ToTensor(image)
+
+        return redirect(url_for('negative'))
     else:
         return flask.render_template('index.html')
+
+
+@app.route('/negative')
+def negative():
+    return flask.render_template('negative.html')
+
+
+@app.route('/positive')
+def positive():
+    return flask.render_template('positive.html')
 
 
 if __name__ == '__main__':
