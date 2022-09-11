02b7b603761f195ef1923ac3f893ac47231336d2
Devam Mondal <devammondal06@gmail.com>
2022-09-11 18:57:10 -0400

    finished front page

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask
+import flask
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['UPLOAD_FOLDER'] = '/uploads'
+app.config['MAX_CONTENT_PATH'] = 10000
+
+@app.route('/', methods = ['GET', 'POST'])
 def main():
-    return "Test"
+    if request.method == 'POST':
+        f = request.files['f']
+        f.save(secure_filename(f.filename))
+        return flask.render_template('finished.html')
+    else:
+        return flask.render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
